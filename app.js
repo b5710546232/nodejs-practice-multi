c41e6dabf9da513e5b326da3534287e084e5f768
b5710546232 <nattapat.su@ku.th>
2016-10-14 08:48:21 +0700

    ep4

--- app.js
+++ app.js
@@ -10,12 +10,61 @@ serv.listen(2000)
 console.log('Server started');
 
 const SOCKET_LIST = []
+const PLAYER_LIST = []
+
+let Player = (id)=>{
+  let self = {
+    x:250,
+    y:250,
+    id:id,
+    number:""+Math.floor(10*Math.random()),
+    pressingRight:false,
+    pressingLeft:false,
+    pressingUp:false,
+    pressingDown:false,
+    maxSpd:10,
+  }
+  self.updatePosition = ()=>{
+    if(self.pressingRight){
+      self.x += self.maxSpd
+    }
+    if(self.pressingLeft){
+      self.x -= self.maxSpd
+    }
+    if(self.pressingDown){
+      self.y += self.maxSpd
+    }
+    if(self.pressingUp){
+      self.y -= self.maxSpd
+    }
+  }
+  return self
+}
+
 const io = require('socket.io')(serv,{})
 io.sockets.on('connection',(socket)=>{
   socket.id = Math.random()
-  socket.x = 0
-  socket.y = 0
   SOCKET_LIST[socket.id] = socket
+
+  let player = Player(socket.id)
+  PLAYER_LIST[socket.id] = player
+
+
+  socket.on('disconnect',()=>{
+    delete SOCKET_LIST[socket.id]
+    delete PLAYER_LIST[socket.id]
+  })
+
+  socket.on('keyPress',(data)=>{
+    if(data.inputId ==='left' )//d
+      player.pressingLeft = data.state
+    if(data.inputId ==='right' )//d
+      player.pressingRight = data.state
+    if(data.inputId ==='up' )//d
+      player.pressingUp = data.state
+    if(data.inputId ==='down' )//d
+      player.pressingDown = data.state
+  })
   console.log('socket connection')
 
   socket.on('happy',(data)=>{
@@ -28,13 +77,19 @@ io.sockets.on('connection',(socket)=>{
 })
 
 setInterval(()=>{
-  for(let i in SOCKET_LIST){
-    let socket = SOCKET_LIST[i]
-    socket.x++
-    socket.y++
-    socket.emit('newPostion',{
-      x:socket.x,
-      y:socket.y
+  let pack = []
+  for(let i in PLAYER_LIST){
+    let player = PLAYER_LIST[i]
+    player.updatePosition()
+    pack.push({
+      x:player.x,
+      y:player.y,
+      number:player.number
     })
   }
+  for(let i in SOCKET_LIST){
+    let socket = SOCKET_LIST[i]
+    socket.emit('newPostion',pack)
+  }
+
 },1000/25)
